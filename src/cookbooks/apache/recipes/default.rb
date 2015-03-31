f1f6d8fec5e858a506099e2ac8e79ff327a48813
Ronald Suez <rsuez93@gmail.com>
2015-03-30 20:50:47 -0400

    changed apache2 default template

--- src/cookbooks/apache/recipes/default.rb
+++ src/cookbooks/apache/recipes/default.rb
@@ -4,3 +4,10 @@ service 'apache2' do
 action :start
 end
 
+template '/etc/apache2/conf-available/serverlimit.conf' do 
+ user 'root'
+ group 'root'
+ mode '0644'
+ notifies :restart, 'service[apache2]'
+end
+
